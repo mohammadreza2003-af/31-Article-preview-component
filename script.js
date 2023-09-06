b74d4a05f5c3424ce21f12f18a4b2d7cd924a364
mohammad2003mrz <gorbanben10@gmail.com>
2023-09-06 21:43:06 +0430

    add script

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+const btn = document.querySelector(".btn");
+const card = document.querySelector(".card");
+btn.addEventListener("click", function () {
+  card.classList.toggle("ac-card");
+});
